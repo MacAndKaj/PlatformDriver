0500cd9ef8f51bf6c37a541fac216cd0bdafdef9
MacAndKaj <mkajdak@gmail.com>
2021-04-07 00:13:24 +0200

    GUI almost done, started working on threaded port handling

--- scripts/MCD_Communicator/src/communicator.py
+++ scripts/MCD_Communicator/src/communicator.py
@@ -23,6 +23,7 @@ class CommunicatorLog(QPlainTextEdit):
         now = datetime.now()
         return now.strftime("%d/%m/%Y-%H:%M:%S.%f")
 
+
 class CommunicatorInputLine(QLineEdit):
     text_signal = QtCore.pyqtSignal(str)
 
@@ -41,34 +42,43 @@ class CommunicatorInputLine(QLineEdit):
         self._input_text = text
 
 
+class ConnectionButton(QPushButton):
+    connection_signal = QtCore.pyqtSignal(str)
 
-def configure_free_communicator() -> QWidget:
-    free_communicator = QWidget()
-    layout = QVBoxLayout()
+    def __init__(self):
+        super().__init__("Connect")
+        self.clicked.connect(self.on_button_clicked)
 
-    communicator_log = CommunicatorLog()
-    communicator_input_line = CommunicatorInputLine()
-    communicator_input_line.text_signal.connect(communicator_log.on_new_tx_text)
+    def on_button_clicked(self):
+        transitions_map = {
+            "Connect": "Disconnect",
+            "Disconnect": "Connect",
+        }
+        previous_text = self.text()
+        self.setText(transitions_map[self.text()])
+        self.connection_signal.emit(previous_text)
 
-    layout.addWidget(communicator_log)
-    layout.addWidget(communicator_input_line)
-
-    free_communicator.setLayout(layout)
-    return free_communicator
+    @pyqtSlot()
+    def on_port_closed(self):
+        print("on_port_closed")
+        self.setText("Connect")
 
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self._communicator_log = None
+        self._communicator_input_line = None
+
         self.setGeometry(0, 0, 600, 400)
         self.setWindowTitle("MCD Communicator")
-        self._connection_button = self.create_connection_button()
+        self._connection_button = ConnectionButton()
         window_layout = QVBoxLayout()
         window_layout.addWidget(self._connection_button)
 
         main_widget = QTabWidget()
-        self._free_communicator_tab = configure_free_communicator()
+        self._free_communicator_tab = self.configure_free_communicator()
         main_widget.addTab(self._free_communicator_tab, "Free Communicator")
         self._message_communicator = QWidget()
         main_widget.addTab(self._message_communicator, "Message Communicator")
@@ -80,17 +90,40 @@ class Window(QMainWindow):
         self.setFocus()
         self.show()
 
-    def create_connection_button(self):
-        connection_button = QPushButton("Connect")
-        connection_button.clicked.connect(self.on_button_clicked)
-        return connection_button
+    def configure_free_communicator(self) -> QWidget:
+        free_communicator = QWidget()
+        layout = QVBoxLayout()
 
-    def on_button_clicked(self):
-        transitions_map = {
-            "Connect": "Disconnect",
-            "Disconnect": "Connect",
-        }
-        self._connection_button.setText(transitions_map[self._connection_button.text()])
+        self._communicator_log = CommunicatorLog()
+        self._communicator_input_line = CommunicatorInputLine()
+        self._communicator_input_line.text_signal.connect(self._communicator_log.on_new_tx_text)
+
+        layout.addWidget(self._communicator_log)
+        layout.addWidget(self._communicator_input_line)
+
+        free_communicator.setLayout(layout)
+        return free_communicator
+
+    def connect_main_window_with_port(self, port_obj: port.Port):
+        self._connection_button.connection_signal.connect(port_obj.execute)
+        self._communicator_input_line.text_signal.connect(port_obj.send)
+        port_obj.port_closed_signal.connect(self._connection_button.on_port_closed)
+
+
+class AppCore:
+    def __init__(self):
+        self._window = Window()
+        self._port = port.Port()
+        self._window.connect_main_window_with_port(self._port)
+        self._port_thread = QThread()
+
+    def start(self):
+        self._port.moveToThread(self._port_thread)
+        self._port_thread.started.connect(self._port.run)
+        self._port_thread.start()
+
+    def stop(self):
+        self._port.stop()
 
 
 if __name__ == '__main__':
@@ -98,6 +131,9 @@ if __name__ == '__main__':
     print(req.serialize())
 
     app = QApplication(sys.argv)
+    app_core = AppCore()
+    # app_core.start()
 
-    window = Window()
-    sys.exit(app.exec_())
+    status = app.exec_()
+    # app_core.stop()
+    sys.exit(status)
